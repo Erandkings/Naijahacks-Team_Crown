32d2ef096f060a8620fd63f8bfed4dcea4af4b7c
Erandkings <Erandkings12@gmail.com>
2019-12-09 22:44:35 +0100

    Add Authentication

---
+++ app/src/main/java/com/example/eduvite/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.example.eduvite;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        TextView textView = (TextView) findViewById(R.id.signup_text);
+        textView.setClickable(true);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), CreateAccount.class));
+            }
+        });
+    }
+}
