d6eaa97240b82ca9ea203e180926994d570c5cd1
SDSSD <oluwasanmiadedaniel@gmail.com>
2019-12-14 11:17:47 +0100

    User Profile added

--- app/src/main/java/com/example/eduvite/DashBoard.java
+++ app/src/main/java/com/example/eduvite/DashBoard.java
@@ -1,17 +1,23 @@
 package com.example.eduvite;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 public class DashBoard extends AppCompatActivity {
 //    private ViewPager viewPager;
+    private View user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        user = (View) findViewById(R.id.imageView6);
 
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(), 4);
@@ -20,6 +26,16 @@ public class DashBoard extends AppCompatActivity {
         adapter.addFragment(new LanguageCourse(), "Language");
         adapter.addFragment(new PhysicsCourse(), "Physics");
         viewPager.setAdapter(adapter);
+
+        user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), UserProfileActivity.class));
+            }
+        });
+
+
+
     }
 
     @Override
